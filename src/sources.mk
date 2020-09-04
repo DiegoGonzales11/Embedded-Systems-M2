8e37c4ed1a8faf715763e891b02f7cb05f45ee21
Diego <161372@unsaac.edu.pe>
2020-09-03 20:08:12 -0500

    Fix some problems

--- src/sources.mk
+++ src/sources.mk
@@ -9,12 +9,19 @@
 #
 #*****************************************************************************
 
-# Add your Source files to this variable
-SOURCES = main.c \
-	  memory.c
+ifeq ($(PLATFORM),MSP432)
+	SOURCES = main.c \
+	   	      memory.c \
+		      interrupts_msp432p401r_gcc.c \
+		      startup_msp432p401r_gcc.c \
+		      system_msp432p401r.c
+   	INCLUDES = -I ../include/common \
+		       -I ../include/CMSIS \
+		       -I ../include/msp432 
+else
+	SOURCES = main.c \
+		  	  memory.c
 
-# Add your include paths to this variable
-INCLUDES = -I ../include/common \
-	   -I ../include/CMSIS \
-	   -I ../include/msp432
+	INCLUDES = -I ../include/common
+endif
 
